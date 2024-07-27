7c9ddd17cad6e9da36d5b503d261c80f82465278
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-27 14:09:46 +0530

    implement basic file routing

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AuthLayout({
+    children,
+}:Readonly<{
+    children: React.ReactNode;
+}>) {
+    return <main>
+        {children}
+    </main>
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function RootLayout({
+    children,
+}:Readonly<{
+    children: React.ReactNode;
+}>) {
+    return <main>
+        SIDEBAR 
+        {children}
+    </main>
+}
